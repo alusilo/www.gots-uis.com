fe5fb02658388379cce71c1fa73a8a36bbf0618b
Alberto Silva <silva.l.a.l@gmail.com>
2021-09-13 16:00:10 -0500

    Removed xtick.labelcolor and ytick.labelcolor due to conflicts with matplotlib version.

--- apps/simulation/gots/system.py
+++ apps/simulation/gots/system.py
@@ -473,8 +473,6 @@ class OpticalSystem(object):
 			'xtick.color': 'white',
 			'ytick.color': 'white',
 			'axes.labelcolor': 'white',
-			'xtick.labelcolor': 'white',
-			'ytick.labelcolor': 'white',
 			'legend.facecolor': 'white',
 			'axes.edgecolor': 'white',
 			'xtick.labelsize': 12,
